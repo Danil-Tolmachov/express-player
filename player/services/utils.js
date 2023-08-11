92ca6a8857c9126323c597f4351c02753b648c98
Danil Tolmachov <danil.tolmachov45@gmail.com>
2023-08-11 19:16:20 +0300

    Add docs to utils.js

--- player/services/utils.js
+++ player/services/utils.js
@@ -7,20 +7,30 @@ const availableFormats = [
   'mp4', 'webm', 'mkv', 'avi'
 ];
 
-
+/*
+* Create unique name
+* @param {string} videoName
+*/
 function generateFileName(videoName) {
   return Date.now() + '_' + videoName;
 }
 
+/*
+* Save file to the storage
+* @param {string} videoName
+*/
 function saveFile(fileName, binaryData) {
-  let fileExtension = fileName.split('.').slice(-1)[0]
+  let fileExtension = fileName.split('.').slice(-1)[0];
   
+  // check if file extension in allowed list
   if (!availableFormats.includes(fileExtension)) {
     return false;
   }
 
+  // save file
   fs.writeFile(path.join(videoPath, generateFileName(fileName)), binaryData, handler);
 
+  // error handler
   function handler(err) {
     if (err) {
       throw Error('Error while uploading file');
@@ -30,6 +40,22 @@ function saveFile(fileName, binaryData) {
   }
 }
 
+/*
+* Get object with all videos data
+* Example: 
+* {
+*  "1": {
+*       "name": "example.mp4",
+*       "src": "\\videos\\141241241235_example.mp4"
+*   },
+*   "2": {
+*       "name": "example2.mp4",
+*       "src": "\\videos\\1691429425261_example2.mp4"
+*   },
+* }
+*
+* @returns {object} - video objects
+*/
 function getVideosSrc() {
   let increment = 0;
   let files = fs.readdirSync(videoPath);
